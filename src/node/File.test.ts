8ee7bac85b23236ea511385937c57b210c94e5dc
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2023-02-16 14:52:37 +0400

    convert tests to mocha with expect

--- src/node/File.test.ts
+++ src/node/File.test.ts
@@ -1,128 +1,203 @@
-import test from 'ava'
+// Convert to mocha with expect
+// import test from 'ava'
 
 import {File} from './File'
 
-test('Takes a name as the second argument', t => {
-  const expected = 'file.txt'
-  const file = new File(['Some content'], expected)
-
-  t.is(file.name, expected)
-})
-
-test('Casts the name argument to string', t => {
-  // @ts-expect-error
-  const file = new File(['Some content'], 42)
-
-  t.is(file.name, '42')
-})
-
-test('The name property keeps its value after being reassigned', t => {
-  const expected = 'file.txt'
-  const file = new File(['Some content'], expected)
-
-  // Browsers won't throw errors in this case,
-  // even though they seem to use the same approach with getters
-  // to make the property read-only. But in Node.js the reassignment will cause an error.
-  // Maybe it's platform specific behaviour?
-  try {
-    // @ts-expect-error
-    file.name = 'another-file.txt'
-  }
-  catch { /* noop */ }
-
-  t.is(file.name, expected)
-})
-
-test('Has the lastModified field', t => {
-  const file = new File(['Some content'], 'file.txt')
-
-  t.is(typeof file.lastModified, 'number')
-})
-
-test('The lastModified property keeps its value after being reassigned', t => {
-  const file = new File(['Some content'], 'file.txt')
-
-  const {lastModified: expected} = file
-
-  try {
-    // @ts-expect-error
-    file.lastModified = Date.now() + 3000
-  }
-  catch { /* noop */ }
-
-  t.is(file.lastModified, expected)
-})
-
-test('Takes the lastModified value from options', t => {
-  const expected = Date.now() + 3000
-  const file = new File(['Some content'], 'file.txt', {lastModified: expected})
-
-  t.is(file.lastModified, expected)
-})
-
-test('Converts Date object in lastModified option to a number', t => {
-  const now = new Date()
-
-  // @ts-expect-error
-  const file = new File(['Some content'], 'file.txt', {lastModified: now})
-
-  t.is(file.lastModified, Number(now))
-})
-
-test('Interpretes undefined value in lastModified option as Date.now()', t => {
-  const lastModified = new File(['Some content'], 'file.txt', {
-    lastModified: undefined,
-  }).lastModified - Date.now()
-
-  t.true(lastModified <= 0 && lastModified >= -20)
-})
-
-test('Interpretes true value in lastModified option as 1', t => {
-  // @ts-expect-error
-  const file = new File(['Some content'], 'file.txt', {lastModified: true})
-
-  t.is(file.lastModified, 1)
-})
-
-test('Interpretes null value in lastModified option as 0', t => {
-  const file = new File(['Some content'], 'file.txt', {lastModified: null})
-
-  t.is(file.lastModified, 0)
-})
-
-test('Interpretes NaN value in lastModified option as 0', t => {
-  t.plan(3)
-
-  const values = ['Not a Number', [], {}]
-
-  // I can't really see anything about this in the spec,
-  // but this is how browsers handle type casting for this option...
-  values.forEach(lastModified => {
-    // @ts-expect-error
-    const file = new File(['Some content'], 'file.txt', {lastModified})
-
-    t.is(file.lastModified, 0)
+describe('File', function () {
+  // test('Takes a name as the second argument', t => {
+  //   const expected = 'file.txt'
+  //   const file = new File(['Some content'], expected)
+  //
+  //   t.is(file.name, expected)
+  // })
+  it('Takes a name as the second argument', function () {
+    const expected = 'file.txt'
+    const file = new File(['Some content'], expected)
+    expect(file.name).toBe(expected)
   })
-})
-
-test('Throws TypeError when constructed with less than 2 arguments', t => {
-  // @ts-expect-error
-  const trap = () => new File(['Some content'])
-
-  t.throws(trap, {
-    instanceOf: TypeError,
-    message   : "Failed to construct 'File': "
-      + '2 arguments required, but only 1 present.',
+  // test('Casts the name argument to string', t => {
+  //   // @ts-expect-error
+  //   const file = new File(['Some content'], 42)
+  //
+  //   t.is(file.name, '42')
+  // })
+  it('Casts the name argument to string', function () {
+    // @ts-expect-error
+    const file = new File(['Some content'], 42)
+    expect(file.name).toBe('42')
   })
-})
-
-test('Throws TypeError when constructed without arguments', t => {
-  // @ts-expect-error
-  const trap = () => new File()
-
-  t.throws(trap, {
-    instanceOf: TypeError,
-    message   : "Failed to construct 'File': "
-      + '2 arguments required, but only 0 present.',
+  // test('The name property keeps its value after being reassigned', t => {
+  //   const expected = 'file.txt'
+  //   const file = new File(['Some content'], expected)
+  //
+  //   // Browsers won't throw errors in this case,
+  //   // even though they seem to use the same approach with getters
+  //   // to make the property read-only. But in Node.js the reassignment will cause an error.
+  //   // Maybe it's platform specific behaviour?
+  //   try {
+  //     // @ts-expect-error
+  //     file.name = 'another-file.txt'
+  //   }
+  //   catch { /* noop */ }
+  //
+  //   t.is(file.name, expected)
+  // })
+  it('The name property keeps its value after being reassigned', function () {
+    const expected = 'file.txt'
+    const file = new File(['Some content'], expected)
+    // Browsers won't throw errors in this case,
+    // even though they seem to use the same approach with getters
+    // to make the property read-only. But in Node.js the reassignment will cause an error.
+    // Maybe it's platform specific behaviour?
+    try {
+      // @ts-expect-error
+      file.name = 'another-file.txt'
+    }
+    catch { /* noop */ }
+    expect(file.name).toBe(expected)
+  })
+  // test('Has the lastModified field', t => {
+  //   const file = new File(['Some content'], 'file.txt')
+  //
+  //   t.is(typeof file.lastModified, 'number')
+  // })
+  it('Has the lastModified field', function () {
+    const file = new File(['Some content'], 'file.txt')
+    expect(typeof file.lastModified).toBe('number')
+  })
+  // test('The lastModified property keeps its value after being reassigned', t => {
+  //   const file = new File(['Some content'], 'file.txt')
+  //
+  //   const {lastModified: expected} = file
+  //
+  //   try {
+  //     // @ts-expect-error
+  //     file.lastModified = Date.now() + 3000
+  //   }
+  //   catch { /* noop */ }
+  //
+  //   t.is(file.lastModified, expected)
+  // })
+  it('The lastModified property keeps its value after being reassigned', function () {
+    const file = new File(['Some content'], 'file.txt')
+    const {lastModified: expected} = file
+    try {
+      // @ts-expect-error
+      file.lastModified = Date.now() + 3000
+    }
+    catch { /* noop */ }
+    expect(file.lastModified).toBe(expected)
+  })
+  // test('Takes the lastModified value from options', t => {
+  //   const expected = Date.now() + 3000
+  //   const file = new File(['Some content'], 'file.txt', {lastModified: expected})
+  //
+  //   t.is(file.lastModified, expected)
+  // })
+  it('Takes the lastModified value from options', function () {
+    const expected = Date.now() + 3000
+    const file = new File(['Some content'], 'file.txt', {lastModified: expected})
+    expect(file.lastModified).toBe(expected)
+  })
+  // test('Converts Date object in lastModified option to a number', t => {
+  //   const now = new Date()
+  //
+  //   // @ts-expect-error
+  //   const file = new File(['Some content'], 'file.txt', {lastModified: now})
+  //
+  //   t.is(file.lastModified, Number(now))
+  // })
+  it('Converts Date object in lastModified option to a number', function () {
+    const now = new Date()
+    // @ts-expect-error
+    const file = new File(['Some content'], 'file.txt', {lastModified: now})
+    expect(file.lastModified).toBe(Number(now))
+  })
+  // test('Interpretes undefined value in lastModified option as Date.now()', t => {
+  //   const lastModified = new File(['Some content'], 'file.txt', {
+  //     lastModified: undefined,
+  //   }).lastModified - Date.now()
+  //
+  //   t.true(lastModified <= 0 && lastModified >= -20)
+  // })
+  it('Interpretes undefined value in lastModified option as Date.now()', function () {
+    const lastModified = new File(['Some content'], 'file.txt', {
+      lastModified: undefined,
+    }).lastModified - Date.now()
+    expect(lastModified <= 0 && lastModified >= -20).toBe(true)
+  })
+  // test('Interpretes true value in lastModified option as 1', t => {
+  //   // @ts-expect-error
+  //   const file = new File(['Some content'], 'file.txt', {lastModified: true})
+  //
+  //   t.is(file.lastModified, 1)
+  // })
+  it('Interpretes true value in lastModified option as 1', function () {
+    // @ts-expect-error
+    const file = new File(['Some content'], 'file.txt', {lastModified: true})
+    expect(file.lastModified).toBe(1)
+  })
+  // test('Interpretes null value in lastModified option as 0', t => {
+  //   const file = new File(['Some content'], 'file.txt', {lastModified: null})
+  //
+  //   t.is(file.lastModified, 0)
+  // })
+  it('Interpretes null value in lastModified option as 0', function () {
+    const file = new File(['Some content'], 'file.txt', {lastModified: null})
+    expect(file.lastModified).toBe(0)
+  })
+  // test('Interpretes NaN value in lastModified option as 0', t => {
+  //   t.plan(3)
+  //
+  //   const values = ['Not a Number', [], {}]
+  //
+  //   // I can't really see anything about this in the spec,
+  //   // but this is how browsers handle type casting for this option...
+  //   values.forEach(lastModified => {
+  //     // @ts-expect-error
+  //     const file = new File(['Some content'], 'file.txt', {lastModified})
+  //
+  //     t.is(file.lastModified, 0)
+  //   })
+  // })
+  it('Interpretes NaN value in lastModified option as 0', function () {
+    const values = ['Not a Number', [], {}]
+    // I can't really see anything about this in the spec,
+    // but this is how browsers handle type casting for this option...
+    values.forEach(lastModified => {
+      // @ts-expect-error
+      const file = new File(['Some content'], 'file.txt', {lastModified})
+      expect(file.lastModified).toBe(0)
+    })
+  })
+  // test('Throws TypeError when constructed with less than 2 arguments', t => {
+  //   // @ts-expect-error
+  //   const trap = () => new File(['Some content'])
+  //
+  //   t.throws(trap, {
+  //     instanceOf: TypeError,
+  //     message   : "Failed to construct 'File': "
+  //       + '2 arguments required, but only 1 present.',
+  //   })
+  // })
+  it('Throws TypeError when constructed with less than 2 arguments', function () {
+    // @ts-expect-error
+    const trap = () => new File(['Some content'])
+    expect(() => trap()).toThrowError(TypeError)
+  })
+  // test('Throws TypeError when constructed without arguments', t => {
+  //   // @ts-expect-error
+  //   const trap = () => new File()
+  //
+  //   t.throws(trap, {
+  //     instanceOf: TypeError,
+  //     message   : "Failed to construct 'File': "
+  //       + '2 arguments required, but only 0 present.',
+  //   })
+  // })
+  it('Throws TypeError when constructed without arguments', function () {
+    // @ts-expect-error
+    const trap = () => new File()
+    expect(() => trap()).toThrowError(TypeError)
   })
 })
--- src/node/fileFromPath.test.ts
+++ src/node/fileFromPath.test.ts
@@ -1,7 +1,8 @@
+// Convert to mocha with expect
 import {stat, readFile, utimes} from 'node:fs/promises'
 import {resolve, basename} from 'node:path'
 
-import test from 'ava'
+// import test from 'ava'
 
 import {File} from './File'
 import {
@@ -14,164 +15,252 @@ import sleep from './__helper__/sleep'
 
 const filePath = resolve('license')
 
-test('Returns File instance', async t => {
-  t.true(await fileFromPath(filePath) instanceof File)
-})
-
-test('sync: Returns File instance', t => {
-  t.true(fileFromPathSync(filePath) instanceof File)
-})
-
-test('Creates a file from path', async t => {
-  const expected: Buffer = await readFile(filePath)
-
-  const file = await fileFromPath(filePath)
-
-  const actual = Buffer.from(await file.arrayBuffer())
-
-  t.true(actual.equals(expected))
-})
-
-test('sync: Creates a file from path', async t => {
-  const expected: Buffer = await readFile(filePath)
-  const file = fileFromPathSync(filePath)
-
-  const actual = Buffer.from(await file.arrayBuffer())
-
-  t.true(actual.equals(expected))
-})
-
-test('Has name taken from file path', async t => {
-  const file = await fileFromPath(filePath)
-
-  t.is<string, string>(file.name, basename(filePath))
-})
-
-test('Has an empty string as file type by default', async t => {
-  const file = await fileFromPath('readme.md')
-
-  t.is<string, string>(file.type, '')
-})
-
-test('Has lastModified field taken from file stats', async t => {
-  const {mtimeMs} = await stat(filePath)
-
-  const file = await fileFromPath(filePath)
-
-  t.is<number, number>(file.lastModified, mtimeMs)
-})
-
-test('Has the size property reflecting the one of the actual file', async t => {
-  const {size} = await stat(filePath)
-
-  const file = await fileFromPath(filePath)
-
-  t.is<number, number>(file.size, size)
-})
-
-test('Allows to set file name as the second argument', async t => {
-  const expected = 'some-file.txt'
-
-  const file = await fileFromPath(filePath, expected)
-
-  t.is<string, string>(file.name, expected)
-})
-
-test('sync: Allows to set file name as the second argument', t => {
-  const expected = 'some-file.txt'
-  const file = fileFromPathSync(filePath, expected)
-
-  t.is<string, string>(file.name, expected)
-})
-
-test('Allows to set file options from second argument', async t => {
-  const expected: FileFromPathOptions = {type: 'text/plain'}
-
-  const file = await fileFromPath(filePath, expected)
-
-  t.deepEqual<FileFromPathOptions, FileFromPathOptions>({
-    type: file.type,
-  }, expected)
-})
-
-test('sync: Allows to set file options from second argument', t => {
-  const expected: FileFromPathOptions = {type: 'text/plain'}
-
-  const file = fileFromPathSync(filePath, expected)
-
-  t.deepEqual<FileFromPathOptions, FileFromPathOptions>({
-    type: file.type,
-  }, expected)
-})
-
-test('Can be read as text', async t => {
-  const expected = await readFile(filePath, 'utf-8')
-  const file = await fileFromPath(filePath)
-
-  const actual = await file.text()
-
-  t.is<string, string>(actual, expected)
-})
-
-test('Can be read as ArrayBuffer', async t => {
-  const expected = await readFile(filePath)
-  const file = await fileFromPath(filePath)
-
-  const actual = await file.arrayBuffer()
-
-  t.true(actual instanceof ArrayBuffer, 'The result must be an ArrayBuffer')
-  t.true(Buffer.from(actual).equals(expected))
-})
-
-test('Can be sliced', async t => {
-  const file = await fileFromPath(filePath)
-
-  const actual = await file.slice(0, 15).text()
-
-  t.is<string, string>(actual, 'The MIT License')
-})
-
-test('Can be sliced from the arbitrary start', async t => {
-  const file = await fileFromPath(filePath)
-
-  const actual = await file.slice(4, 15).text()
-
-  t.is<string, string>(actual, 'MIT License')
-})
-
-test('Can be sliced from Blob returned from .slice() method', async t => {
-  const license = new File([await readFile(filePath)], 'license')
-  const file = await fileFromPath(filePath)
-
-  const expected = license.slice(4, 11).slice(2, 5)
-  const actual = file.slice(4, 11).slice(2, 5)
-
-  t.is<string, string>(await actual.text(), await expected.text())
-})
-
-test('Reads from empty file', async t => {
-  const file = await fileFromPath(filePath)
-
-  const sliced = file.slice(0, 0)
-
-  t.is<number, number>(sliced.size, 0, 'Must have 0 size')
-  t.is<string, string>(await sliced.text(), '', 'Must return empty string')
-})
-
-test('Fails attempt to read modified file', async t => {
-  const path = resolve('readme.md')
-  const file = await fileFromPath(path)
-
-  await sleep(100) // wait 100ms
-
-  const now = new Date()
-
-  await utimes(path, now, now)
-
-  await t.throwsAsync(() => file.text(), {
-    name   : 'NotReadableError',
-    message: 'The requested file could not be read, '
-      + 'typically due to permission problems that have occurred '
-      + 'after a reference to a file was acquired.',
+describe('fileFromPath', function () {
+  // test('Returns File instance', async t => {
+  //   t.true(await fileFromPath(filePath) instanceof File)
+  // })
+  //
+  // test('sync: Returns File instance', t => {
+  //   t.true(fileFromPathSync(filePath) instanceof File)
+  // })
+  it('Returns File instance', async function () {
+    expect(await fileFromPath(filePath)).toBeInstanceOf(File)
+  })
+  // test('Creates a file from path', async t => {
+  //   const expected: Buffer = await readFile(filePath)
+  //
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const actual = Buffer.from(await file.arrayBuffer())
+  //
+  //   t.true(actual.equals(expected))
+  // })
+  it('Creates a file from path', async function () {
+    const expected: Buffer = await readFile(filePath)
+    const file = await fileFromPath(filePath)
+    const actual = Buffer.from(await file.arrayBuffer())
+    expect(actual.equals(expected)).toBe(true)
+  })
+  // test('sync: Creates a file from path', async t => {
+  //   const expected: Buffer = await readFile(filePath)
+  //   const file = fileFromPathSync(filePath)
+  //
+  //   const actual = Buffer.from(await file.arrayBuffer())
+  //
+  //   t.true(actual.equals(expected))
+  // })
+  it('sync: Creates a file from path', async function () {
+    const expected: Buffer = await readFile(filePath)
+    const file = fileFromPathSync(filePath)
+    const actual = Buffer.from(await file.arrayBuffer())
+    expect(actual.equals(expected)).toBe(true)
+  })
+  // test('Has name taken from file path', async t => {
+  //   const file = await fileFromPath(filePath)
+  //
+  //   t.is<string, string>(file.name, basename(filePath))
+  // })
+  it('Has name taken from file path', async function () {
+    const file = await fileFromPath(filePath)
+    expect(file.name).toBe(basename(filePath))
+  })
+  // test('Has an empty string as file type by default', async t => {
+  //   const file = await fileFromPath('readme.md')
+  //
+  //   t.is<string, string>(file.type, '')
+  // })
+  it('Has an empty string as file type by default', async function () {
+    const file = await fileFromPath('readme.md')
+    expect(file.type).toBe('')
+  })
+  // test('Has lastModified field taken from file stats', async t => {
+  //   const {mtimeMs} = await stat(filePath)
+  //
+  //   const file = await fileFromPath(filePath)
+  //
+  //   t.is<number, number>(file.lastModified, mtimeMs)
+  // })
+  it('Has lastModified field taken from file stats', async function () {
+    const {mtimeMs} = await stat(filePath)
+    const file = await fileFromPath(filePath)
+    expect(file.lastModified).toBe(mtimeMs)
+  })
+  // test('Has the size property reflecting the one of the actual file', async t => {
+  //   const {size} = await stat(filePath)
+  //
+  //   const file = await fileFromPath(filePath)
+  //
+  //   t.is<number, number>(file.size, size)
+  // })
+  it('Has the size property reflecting the one of the actual file', async function () {
+    const {size} = await stat(filePath)
+    const file = await fileFromPath(filePath)
+    expect(file.size).toBe(size)
+  })
+  // test('Allows to set file name as the second argument', async t => {
+  //   const expected = 'some-file.txt'
+  //
+  //   const file = await fileFromPath(filePath, expected)
+  //
+  //   t.is<string, string>(file.name, expected)
+  // })
+  it('Allows to set file name as the second argument', async function () {
+    const expected = 'some-file.txt'
+    const file = await fileFromPath(filePath, expected)
+    expect(file.name).toBe(expected)
+  })
+  // test('sync: Allows to set file name as the second argument', t => {
+  //   const expected = 'some-file.txt'
+  //   const file = fileFromPathSync(filePath, expected)
+  //
+  //   t.is<string, string>(file.name, expected)
+  // })
+  it('sync: Allows to set file name as the second argument', function () {
+    const expected = 'some-file.txt'
+    const file = fileFromPathSync(filePath, expected)
+    expect(file.name).toBe(expected)
+  })
+  // test('Allows to set file options from second argument', async t => {
+  //   const expected: FileFromPathOptions = {type: 'text/plain'}
+  //
+  //   const file = await fileFromPath(filePath, expected)
+  //
+  //   t.deepEqual<FileFromPathOptions, FileFromPathOptions>({
+  //     type: file.type,
+  //   }, expected)
+  // })
+  it('Allows to set file options from second argument', async function () {
+    const expected: FileFromPathOptions = {type: 'text/plain'}
+    const file = await fileFromPath(filePath, expected)
+    expect({
+      type: file.type,
+    }).toEqual(expected)
+  })
+  // test('sync: Allows to set file options from second argument', t => {
+  //   const expected: FileFromPathOptions = {type: 'text/plain'}
+  //
+  //   const file = fileFromPathSync(filePath, expected)
+  //
+  //   t.deepEqual<FileFromPathOptions, FileFromPathOptions>({
+  //     type: file.type,
+  //   }, expected)
+  // })
+  it('sync: Allows to set file options from second argument', function () {
+    const expected: FileFromPathOptions = {type: 'text/plain'}
+    const file = fileFromPathSync(filePath, expected)
+    expect({
+      type: file.type,
+    }).toEqual(expected)
+  })
+  // test('Can be read as text', async t => {
+  //   const expected = await readFile(filePath, 'utf-8')
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const actual = await file.text()
+  //
+  //   t.is<string, string>(actual, expected)
+  // })
+  it('Can be read as text', async function () {
+    const expected = await readFile(filePath, 'utf-8')
+    const file = await fileFromPath(filePath)
+    const actual = await file.text()
+    expect(actual).toBe(expected)
+  })
+  // test('Can be read as ArrayBuffer', async t => {
+  //   const expected = await readFile(filePath)
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const actual = await file.arrayBuffer()
+  //
+  //   t.true(actual instanceof ArrayBuffer, 'The result must be an ArrayBuffer')
+  //   t.true(Buffer.from(actual).equals(expected))
+  // })
+  it('Can be read as ArrayBuffer', async function () {
+    const expected = await readFile(filePath)
+    const file = await fileFromPath(filePath)
+    const actual = await file.arrayBuffer()
+    expect(actual instanceof ArrayBuffer).toBe(true)
+    expect(Buffer.from(actual).equals(expected)).toBe(true)
+  })
+  // test('Can be sliced', async t => {
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const actual = await file.slice(0, 15).text()
+  //
+  //   t.is<string, string>(actual, 'The MIT License')
+  // })
+  it('Can be sliced', async function () {
+    const file = await fileFromPath(filePath)
+    const actual = await file.slice(0, 15).text()
+    expect(actual).toBe('The MIT License')
+  })
+  // test('Can be sliced from the arbitrary start', async t => {
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const actual = await file.slice(4, 15).text()
+  //
+  //   t.is<string, string>(actual, 'MIT License')
+  // })
+  it('Can be sliced from the arbitrary start', async function () {
+    const file = await fileFromPath(filePath)
+    const actual = await file.slice(4, 15).text()
+    expect(actual).toBe('MIT License')
+  })
+  // test('Can be sliced from Blob returned from .slice() method', async t => {
+  //   const license = new File([await readFile(filePath)], 'license')
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const expected = license.slice(4, 11).slice(2, 5)
+  //   const actual = file.slice(4, 11).slice(2, 5)
+  //
+  //   t.is<string, string>(await actual.text(), await expected.text())
+  // })
+  it('Can be sliced from Blob returned from .slice() method', async function () {
+    const license = new File([await readFile(filePath)], 'license')
+    const file = await fileFromPath(filePath)
+    const expected = license.slice(4, 11).slice(2, 5)
+    const actual = file.slice(4, 11).slice(2, 5)
+    expect(await actual.text()).toBe(await expected.text())
+  })
+  // test('Reads from empty file', async t => {
+  //   const file = await fileFromPath(filePath)
+  //
+  //   const sliced = file.slice(0, 0)
+  //
+  //   t.is<number, number>(sliced.size, 0, 'Must have 0 size')
+  //   t.is<string, string>(await sliced.text(), '', 'Must return empty string')
+  // })
+  it('Reads from empty file', async function () {
+    const file = await fileFromPath(filePath)
+    const sliced = file.slice(0, 0)
+    expect(sliced.size).toBe(0)
+    expect(await sliced.text()).toBe('')
+  })
+  // test('Fails attempt to read modified file', async t => {
+  //   const path = resolve('readme.md')
+  //   const file = await fileFromPath(path)
+  //
+  //   await sleep(100) // wait 100ms
+  //
+  //   const now = new Date()
+  //
+  //   await utimes(path, now, now)
+  //
+  //   await t.throwsAsync(() => file.text(), {
+  //     name   : 'NotReadableError',
+  //     message: 'The requested file could not be read, '
+  //       + 'typically due to permission problems that have occurred '
+  //       + 'after a reference to a file was acquired.',
+  //   })
+  // })
+  it('Fails attempt to read modified file', async function () {
+    const path = resolve('readme.md')
+    const file = await fileFromPath(path)
+    await sleep(100) // wait 100ms
+    const now = new Date()
+    await utimes(path, now, now)
+    await expect(() => file.text())
+      .rejects
+      .toThrowError('The requested file could not be read, typically due to permission problems that have occurred after a reference to a file was acquired.')
   })
 })
